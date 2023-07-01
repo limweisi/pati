1c1b4d187622152b6c49a0b9b1cee09342a6f365
Pureeeeen <112765341+Pureeeeen@users.noreply.github.com>
2023-07-01 15:56:50 +0800

    Reposition the contents
    Reposition the div containers and text according to figma. Added font size and font thickness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -128,35 +128,35 @@ const Home: NextPage = () => {
       </div>
       <div className="bg-gradient-to-br from-pink-200 to-pink-50 h-1044px flex justify-around flex-row">
         <div className="pt-10 flex flex-col">
-          <img src="/content_img.jpg" className="h-576px w-80"></img>
-          <div className="content-info content-center">
-            <h1 className="pt-10 text-pink-600 text-2xl font-medium ">Name of item 1</h1>
+          <img src="/content_img.jpg" className="basis-1/2 h-576px w-80"></img>
+          <div className="basis-1/4 content-info text-center">
+            <h1 className="pt-10 text-pink-500 text-4xl ">Name of item 1</h1>
+            <p className="text-purple-500 text-xl">Description of item</p>
+          </div>
+          <button className=" mx-9 rounded-full border-solid border-2 border-black bg-orange-200 ">Get Now</button>
+        </div>
+        <div className="pt-10 flex flex-col ">
+          <img src="/content_img.jpg" className="basis-1/2 h-576px w-80"></img>
+          <div className="basis-1/4 content-info text-center ">
+            <h1 className="pt-10 text-pink-500 text-4xl px-14">Name of item 2</h1>
             <p className="text-purple-500 text-xl">Description of item</p>
           </div>
           <button className="mx-9 rounded-full border-solid border-2 border-black bg-orange-200 ">Get Now</button>
         </div>
         <div className="pt-10 flex flex-col">
-          <img src="/content_img.jpg" className="h-576px w-80"></img>
-          <div className="content-info content-center">
-            <h1 className="pt-10 text-pink-600 text-2xl font-medium">Name of item 2</h1>
-            <p className="text-purple-500 text-xl">Description of item</p>
-          </div>
-          <button className="mx-9 rounded-full border-solid border-2 border-black bg-orange-200 ">Get Now</button>
-        </div>
-        <div className="pt-10 flex flex-col">
-          <img src="/content_img.jpg" className="h-576px w-80"></img>
-          <div className="content-info content-center">
-            <h1 className="pt-10 text-pink-600 text-2xl font-medium">Name of item 3</h1>
+          <img src="/content_img.jpg" className="basis-1/2 h-576px w-80"></img>
+          <div className="content-info text-center basis-1/4">
+            <h1 className="pt-10 text-pink-500 text-4xl">Name of item 3</h1>
             <p className="text-purple-500 text-xl">Description of item</p>
           </div>
           <button className="mx-9 rounded-full border-solid border-2 border-black bg-orange-200 ">Get Now</button>
         </div>
       </div>
-      <div className="bg-content-background bg-cover bg-center bg-no-repeat h-96">
-        <div className="w-1/3 absolute right-0 my-auto" >
-          <h1 className="pt-10 text-pink-600 text-2xl font-bold">PASTRIES FOR ANY OCCASION!</h1>
-          <p>Both Savory and Sweet options - we have them both</p>
-          <p>Do you have a sweet tooth? Vote Now!</p>
+      <div className="bg-content-background bg-cover bg-center bg-no-repeat h-96 relative">
+        <div className="float-right text-right pr-96 text-base absolute bottom-32 right-10" >
+          <h1 className="pt-10 text-pink-600 text-4xl font-bold">PASTRIES FOR ANY OCCASION!</h1>
+          <p className="text-purple-500 text-lg pt-10">Both Savory and Sweet options - we have them both</p>
+          <p className="text-purple-500 text-lg pt-10">Do you have a sweet tooth? Vote Now!</p>
         </div>
       </div>
       <footer className="bg-home-background bg-cover bg-center bg-no-repeat h-48 ">
